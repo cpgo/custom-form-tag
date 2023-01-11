0d1433e495227e7fae9fdc3c8a3a9b571f6ad582
Cassio Godinho <cassiopgodinho@gmail.com>
2023-01-10 22:04:57 -0300

    create hidden input with operation

--- src/search_input.ts
+++ src/search_input.ts
@@ -1,12 +1,30 @@
 export class SearchInput extends HTMLInputElement {
-
   $(selector: string) {
     return this.shadowRoot && this.shadowRoot.querySelector(selector)
   }
+  constructor() {
+    super();
+    this.baseName = this.name
+    this.name = `${this.baseName}[value]`
+    this.insert()
+  }
+
+  get op() {
+    const attr = this.getAttribute('op')
+    if (!attr) {
+      throw new Error("op is required");
+
+    }
+    return attr
+  }
 
 
-constructor() {
-	super();
-}
+  insert() {
+    const input = document.createElement('input')
+    input.type = 'hidden'
+    input.name = `${this.baseName}[op]`
+    input.value = this.op
+    this.insertAdjacentElement("afterend", input)
+  }
 
 }
