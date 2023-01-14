afa109fb5d7bc975400430eed21f3e9760737b47
Cassio Godinho <cassiopgodinho@gmail.com>
2023-01-14 20:01:40 -0300

    test params object

--- tests/parse.spec.ts
+++ tests/parse.spec.ts
@@ -1,10 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page, baseURL }) => {
+test('has correct url params', async ({ page, baseURL }) => {
   await page.goto('/');
 
   await expect(page).toHaveTitle(/Document/);
   await page.getByText("Save").click()
-  expect(page.url()).toBe(`${baseURL}/index.html?name=&price=&raw=`)
-
+  const urlString = `${baseURL}/index.html?name%5Bvalue%5D=&name%5Bop%5D=eq&price%5Bvalue%5D=&price%5Bop%5D=gt&raw=`
+  const url = new URL(urlString)
+  expect(page.url()).toBe(urlString)
+  const params = url.searchParams
+  const expectedParamsObject = {
+    "name[value]": "",
+    "name[op]": "eq",
+    "price[value]": "",
+    "price[op]": "gt",
+    "raw": ""
+  }
+  debugger
+  const expectedParams = new URLSearchParams(expectedParamsObject)
+  expect(params).toEqual(expectedParams)
 });
